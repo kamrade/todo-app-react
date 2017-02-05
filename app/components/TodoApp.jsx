845fd5a3c9682e24e6ad1aa8f804905b5b71f3cc
Denis Michailov <kamrad.reg@gmail.com>
2017-02-05 17:34:28 +1000

    sync all todos with localStorage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,77 +1,61 @@
 var React    = require('react');
+var uuid = require('node-uuid');
+
 var TodoList = require('TodoList');
 var AddTodo  = require('AddTodo');
 var TodoSearch = require('TodoSearch');
-var uuid = require('node-uuid');
+var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-	getInitialState: function() {
-		return {
-			showCompleted: false,
-			searchText: '',
-			todos: [
-				{
-					id: uuid(),
-					text: 'Walk the dog',
-					completed: false
-				},
-				{
-					id: uuid(),
-					text: 'Clean the yard',
-					completed: true
-				},
-				{
-					id: uuid(),
-					text: 'Leave mail on porch',
-					completed: true
-				},
-				{
-					id: uuid(),
-					text: 'Play video games',
-					completed: false
-				}
-			]
-		};
-	},
-	handleAddTodo: function(text) {
-		console.log('new todo: ' + text);
-		this.setState({
-			todos: [
-				...this.state.todos,
-				{
-					text: text,
-					id: uuid(),
-					completed: false
-				}
-			]
-		})
-	},
-	handleToggle: function(id) {
-		var updatedTodos = this.state.todos.map((todo) => {
-			if(todo.id === id) {
-				todo.completed = !todo.completed;
-			}
-			return todo;
-		});
-		this.setState({ todos: updatedTodos });
-	},
-	handleSearch: function(showCompleted, searchText) {
-		this.setState({
-			showCompleted: showCompleted,
-			searchText: searchText.toLowerCase()
-		});
-	},
-	render: function() {
-		var { todos } = this.state;
+  getInitialState: function() {
+    return {
+      showCompleted: false,
+      searchText: '',
+      todos: TodoAPI.getTodos()
+    };
+  },
+  componentDidUpdate: function() {
+    TodoAPI.setTodos(this.state.todos);
+  },
+  handleAddTodo: function(text) {
+    console.log('new todo: ' + text);
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          text: text,
+          id: uuid(),
+          completed: false
+        }
+      ]
+    })
+  },
+  handleToggle: function(id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    this.setState({ todos: updatedTodos });
+  },
+  handleSearch: function(showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
+  render: function() {
+    var { todos } = this.state;
 
-		return (
-			<div>
-				<TodoSearch onSearch={this.handleSearch} />
-				<TodoList todos={todos} onToggle={this.handleToggle} />
-				<AddTodo onAddTodo={this.handleAddTodo} />
-			</div>
-		);
-	}
+    return (
+      <div>
+        <TodoSearch onSearch={this.handleSearch} />
+        <TodoList todos={todos} onToggle={this.handleToggle} />
+        <AddTodo onAddTodo={this.handleAddTodo} />
+      </div>
+    );
+  }
 });
 
 module.exports = TodoApp;
