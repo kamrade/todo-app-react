add922e0ffc24ec559b688b2c88b2262d3071bb5
Denis Michailov <kamrad.reg@gmail.com>
2017-02-21 23:21:14 +1000

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -3,41 +3,54 @@ var $ = require('jquery');
 module.exports = {
 
 
-  setTodos: function(todos) {
-    if($.isArray(todos)) {
-      localStorage.setItem('todos', JSON.stringify(todos));
-      return todos;
+    setTodos: function(todos) {
+        if($.isArray(todos)) {
+            localStorage.setItem('todos', JSON.stringify(todos));
+            return todos;
+        }
+    },
+
+
+    getTodos: function() {
+        var stringTodos = localStorage.getItem('todos');
+        var todos = [];
+
+        try {
+            todos = JSON.parse(stringTodos);
+        } catch(e) {
+
+        }
+
+        return $.isArray(todos) ? todos : [];
+    },
+
+    filterTodos: function(todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        // filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+
+        // filter by searchText
+        filteredTodos = filteredTodos.filter((todo) => {
+            var text = todo.text.toLowerCase();
+            return searchText.length === 0 || text.indexOf(searchText) > -1;
+        });
+
+        // Sort todos with non-completed first
+        filteredTodos.sort((a, b) => {
+            if (!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        return filteredTodos;
     }
-  },
-
-
-  getTodos: function() {
-    var stringTodos = localStorage.getItem('todos');
-    var todos = [];
-
-    try {
-      todos = JSON.parse(stringTodos);
-    } catch(e) {
-
-    }
-
-    return $.isArray(todos) ? todos : [];
-  },
-
-  filterTodos: function(todos, showCompleted, searchText) {
-    var filteredTodos = todos;
-
-    // filter by showCompleted
-    filteredTodos = filteredTodos.filter((todo) => {
-      return !todo.completed || showCompleted;
-    });
-
-    // filter by searchText
-
-    // Sort todos with non-completed first
-
-    return filteredTodos;
-  }
 
 
 }
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -82,6 +82,21 @@ describe('TodoAPI', () => {
       expect(filteredTodos.length).toBe(1);
     });
 
+    it('should sort by completed status', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by seach text', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+        expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should return all todos if search text is empty', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos.length).toBe(3);
+    });
+
   });
 
 });
