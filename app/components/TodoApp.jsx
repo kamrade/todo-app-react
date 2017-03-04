7d21d0867d7e4e6ee1d146aba9aba078041af931
Denis Michailov <kamrad.reg@gmail.com>
2017-03-04 18:23:43 +1000

    Style Todo Application

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -8,59 +8,67 @@ var TodoSearch = require('TodoSearch');
 var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-  getInitialState: function() {
-    return {
-      showCompleted: false,
-      searchText: '',
-      todos: TodoAPI.getTodos()
-    };
-  },
-  componentDidUpdate: function() {
-    TodoAPI.setTodos(this.state.todos);
-  },
-  handleAddTodo: function(text) {
-    console.log('new todo: ' + text);
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        {
-          text: text,
-          id: uuid(),
-          completed: false,
-          createdAt: moment().unix(),
-          completedAt: undefined
-        }
-      ]
-    })
-  },
-  handleToggle: function(id) {
-    var updatedTodos = this.state.todos.map((todo) => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed;
-        todo.completedAt = todo.completed ? moment().unix() : undefined;
-      }
-      return todo;
-    });
-    this.setState({ todos: updatedTodos });
-  },
-  handleSearch: function(showCompleted, searchText) {
-    this.setState({
-      showCompleted: showCompleted,
-      searchText: searchText.toLowerCase()
-    });
-  },
-  render: function() {
-    var { todos, showCompleted, searchText } = this.state;
-    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+    getInitialState: function() {
+        return {
+            showCompleted: false,
+            searchText: '',
+            todos: TodoAPI.getTodos()
+        };
+    },
+    componentDidUpdate: function() {
+        TodoAPI.setTodos(this.state.todos);
+    },
+    handleAddTodo: function(text) {
+        console.log('new todo: ' + text);
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    text: text,
+                    id: uuid(),
+                    completed: false,
+                    createdAt: moment().unix(),
+                    completedAt: undefined
+                }
+            ]
+        })
+    },
+    handleToggle: function(id) {
+        var updatedTodos = this.state.todos.map((todo) => {
+            if(todo.id === id) {
+                todo.completed = !todo.completed;
+                todo.completedAt = todo.completed ? moment().unix() : undefined;
+            }
+            return todo;
+        });
+        this.setState({ todos: updatedTodos });
+    },
+    handleSearch: function(showCompleted, searchText) {
+        this.setState({
+            showCompleted: showCompleted,
+            searchText: searchText.toLowerCase()
+        });
+    },
+    render: function() {
+        var { todos, showCompleted, searchText } = this.state;
+        var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
-    return (
-      <div className="app-wrapper">
-        <TodoSearch onSearch={this.handleSearch} />
-        <TodoList todos={filteredTodos} onToggle={this.handleToggle} />
-        <AddTodo onAddTodo={this.handleAddTodo} />
-      </div>
-    );
-  }
+        return (
+            <div className="app-wrapper">
+                <h1 className="page-title">Todo App</h1>
+                <div className="row">
+                    <div className="column small-centered small-11 medium-6 large-5">
+                        <div className="container">
+                            <TodoSearch onSearch={this.handleSearch} />
+                            <TodoList todos={filteredTodos} onToggle={this.handleToggle} />
+                            <AddTodo onAddTodo={this.handleAddTodo} />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
 });
 
 module.exports = TodoApp;
--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -8,23 +8,31 @@ var TodoList = require('TodoList');
 var Todo     = require('Todo');
 
 describe('TodoList', () => {
-	it('should exist', () => {
-		expect(TodoList).toExist();
-	});
+    it('should exist', () => {
+        expect(TodoList).toExist();
+    });
 
-	it('should render one Todo component for each of todo item', () => {
-		var todos = [{
-			id: 1,
-			text: 'Do Something'
-		}, {
-			id: 2,
-			text: 'Check Mail'
-		}];
+    it('should render one Todo component for each of todo item', () => {
+        var todos = [{
+            id: 1,
+            text: 'Do Something'
+        }, {
+            id: 2,
+            text: 'Check Mail'
+        }];
 
-		var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
-		var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+        var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
 
-		expect(todosComponents.length).toBe(todos.length);
+        expect(todosComponents.length).toBe(todos.length);
 
-	});
+    });
+
+    it('should render empty message if no todos', () => {
+        var todos = [];
+        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+        var $el = $(ReactDOM.findDOMNode(todoList));
+
+        expect($el.find(".container__message").length).toBe(1);
+    });
 });
