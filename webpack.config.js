ec6d00622526d57298a36a4b82e1d4c2e383b1a3
Denis Michailov <kamrad.reg@gmail.com>
2017-06-18 21:57:43 +1000

    Add redux store

--- webpack.config.js
+++ webpack.config.js
@@ -5,74 +5,75 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
 
 module.exports = {
-	entry: [
-		'script!jquery/dist/jquery.min.js',
-		'script!foundation-sites/dist/js/foundation.min.js',
-		'./app/app.jsx'
-	],
-	externals: {
-		jquery: 'jQuery'
-	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			'$': 'jquery',
-			'jQuery': 'jquery'
-		}),
-		new webpack.DefinePlugin({
-			NODE_ENV: JSON.stringify(NODE_ENV),
-			LANG: JSON.stringify('en')
-		})
-	],
-	output: {
-		path: __dirname,
-		filename: './public/bundle.js'
-	},
-	resolve: {
-		root: __dirname,
-		modulesDirectories: [
-			'node_modules',
-			'./app/components',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/js/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    }),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV),
+      LANG: JSON.stringify('en')
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './app/components',
       './app/api'
-		],
-		alias: {
-			applicationStyles: 'app/styles/app.scss',
-			actions: 'app/actions/actions.jsx',
-			reducers: 'app/reducers/reducers.jsx'
-		},
-		extensions: ['.js', '', '.jsx']
-	},
-	// watch: NODE_ENV == 'development',
-	watchOptions: {
-		aggregateTimeout: 100
-	},
-	devtool: NODE_ENV == 'development' ? 'inline-source-map' : null,
-	module: {
-		loaders: [
-			{
-				loader: 'babel-loader',
-				query: {
-					presets: ['react', 'es2015', 'stage-0']
-				},
-				test: /\.jsx?$/,
-				exclude: /(node_modules|lib)/
-			}
-		]
-	},
-	sassLoader: {
-		includePaths: [
-			path.resolve(__dirname, './node_modules/foundation-sites/scss')
-		]
-	}
+    ],
+    alias: {
+      applicationStyles: 'app/styles/app.scss',
+      actions: 'app/actions/actions.jsx',
+      reducers: 'app/reducers/reducers.jsx',
+      configureStore: 'app/store/configureStore.jsx'
+    },
+    extensions: ['.js', '', '.jsx']
+  },
+  // watch: NODE_ENV == 'development',
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+  devtool: NODE_ENV == 'development' ? 'inline-source-map' : null,
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|lib)/
+      }
+    ]
+  },
+  sassLoader: {
+    includePaths: [
+      path.resolve(__dirname, './node_modules/foundation-sites/scss')
+    ]
+  }
 };
 
 if (NODE_ENV == 'production') {
-	module.exports.plugins.push(
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false,
-				drop_console: true,
-				unsafe: true
-			}
-		})
-	);
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: true,
+        unsafe: true
+      }
+    })
+  );
 };
